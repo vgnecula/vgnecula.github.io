2b5595085bb46e1dc00280e30c18dbdb4d94aa4d
vgnecula <vg.necula@gmail.com>
2023-11-24 18:23:42 -0500

    DFDS

--- script.js
+++ script.js
@@ -21,24 +21,27 @@ function drawText(text, x, y, opacity, fontSize) {
     ctx.fillText(text, x, y);
 }
 
-function drawFlickeringCursor(x, y, visible) {
+function drawPixelatedCursor(x, y, visible) {
     if (visible) {
         ctx.fillStyle = 'rgba(255, 255, 255, 1)';
         ctx.fillRect(x, y - 15, 2, 20);
+        ctx.fillRect(x + 2, y - 15, 2, 20);
+        ctx.fillRect(x + 4, y - 15, 2, 20);
     }
 }
 
-function animateTextWithFlickeringCursor(text, x, y, opacity, targetElement, fontSize) {
+function animateTextWithPixelatedCursor(text, x, y, opacity, targetElement, fontSize) {
     let index = 0;
     let cursorX = x;
+    let cursorY = y - 15;
     let cursorVisible = true;
 
     function typeNextLetter() {
         if (index <= text.length) {
             drawText(text.substring(0, index), x, y, opacity, fontSize);
-            drawFlickeringCursor(cursorX, y, cursorVisible);
+            drawPixelatedCursor(cursorX, cursorY, cursorVisible);
             cursorVisible = !cursorVisible;
-            cursorX += ctx.measureText(text[index]).width + 10; // Adjusted for better spacing
+            cursorX += ctx.measureText(text[index]).width; // Adjusted for better spacing
             index++;
 
             // Increase opacity gradually
@@ -58,10 +61,10 @@ function animateTextWithFlickeringCursor(text, x, y, opacity, targetElement, fon
     }, 2000); // Adjust the delay as needed
 }
 
-// Start typing animation for the name with flickering cursor
-animateTextWithFlickeringCursor(nameText, canvas.width / 2 - 80, canvas.height / 2 - 20, 0, nameElement, 36);
+// Start typing animation for the name with pixelated cursor
+animateTextWithPixelatedCursor(nameText, canvas.width / 2 - 80, canvas.height / 2 - 20, 0, nameElement, 36);
 
-// Start typing animation for the subtitle with flickering cursor after a delay
+// Start typing animation for the subtitle with pixelated cursor after a delay
 setTimeout(() => {
-    animateTextWithFlickeringCursor(subtitleText, canvas.width / 2 - 80, canvas.height / 2 + 40, 0, subtitleElement, 18);
+    animateTextWithPixelatedCursor(subtitleText, canvas.width / 2 - 80, canvas.height / 2 + 40, 0, subtitleElement, 18);
 }, 5000); // Adjust the delay between animations as needed
