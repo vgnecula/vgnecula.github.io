d3c29c68a69bc7ffcd8ebf408f5d641172cc9fdd
vgnecula <vg.necula@gmail.com>
2023-11-24 17:30:21 -0500

    cursor

--- script.js
+++ script.js
@@ -10,21 +10,21 @@ const subtitleElement = document.getElementById('subtitle');
 nameElement.style.opacity = 0;
 subtitleElement.style.opacity = 0;
 
-function drawText(text, x, y, opacity, targetElement) {
+function drawTextWithCursor(text, x, y, opacity, targetElement, cursorVisible) {
     ctx.fillStyle = `rgba(255, 255, 255, ${opacity})`;
     ctx.font = '20px Courier New';
-    ctx.fillText(text, x, y);
+    ctx.fillText(text + (cursorVisible ? '|' : ''), x, y);
 }
 
-function animateText(text, x, y, opacity, targetElement) {
+function animateTextWithCursor(text, x, y, opacity, targetElement) {
     let index = 0;
+    let cursorVisible = true;
 
     function typeNextLetter() {
-        if (index < text.length) {
-            targetElement.textContent = text.substring(0, index + 1);
+        if (index <= text.length) {
+            drawTextWithCursor(text.substring(0, index), x, y, opacity, targetElement, cursorVisible);
+            cursorVisible = !cursorVisible;
             index++;
-            opacity += 0.1; // Increase opacity gradually
-            targetElement.style.opacity = opacity; // Set the opacity
             setTimeout(typeNextLetter, 100); // Adjust the typing speed by changing the timeout
         }
     }
@@ -37,9 +37,9 @@ function animateText(text, x, y, opacity, targetElement) {
 }
 
 // Start typing animation for the name
-animateText(nameText, canvas.width / 2 - 80, canvas.height / 2 - 20, 0, nameElement);
+animateTextWithCursor(nameText, canvas.width / 2 - 80, canvas.height / 2 - 20, 0, nameElement);
 
 // Start typing animation for the subtitle after a delay
 setTimeout(() => {
-    animateText(subtitleText, canvas.width / 2 - 80, canvas.height / 2 + 20, 0, subtitleElement);
-}, 3000); // Adjust the delay between animations as needed
+    animateTextWithCursor(subtitleText, canvas.width / 2 - 80, canvas.height / 2 + 20, 0, subtitleElement);
+}, 1500); // Adjust the delay between animations as needed
